e12483f2b4871583f0ae61a098ae6887ef3cd7b9
Rohail Najam <rohail_najam@hotmail.com>
2017-06-17 13:19:22 +0500

    Added CSV Parser

---
+++ app/renderers/openfile.js
@@ -0,0 +1,23 @@
+/**
+ * Created by Rohail on 6/17/2017.
+ */
+
+const ipc = require('electron').ipcRenderer
+const csvParser = require('csvtojson');
+
+const selectDirBtn = document.getElementById('fileClicker')
+
+selectDirBtn.addEventListener('click', function (event) {
+  ipc.send('open-file-dialog')
+});
+let data = [];
+ipc.on('selected-directory', function (event, path) {
+  csvParser()
+    .fromFile(path[0])
+    .on('json',(jsonObj)=>{
+      data.push(jsonObj);
+    })
+    .on('done',async (error)=>{
+      document.getElementById('selected-file').innerHTML = `You selected: ${data}`
+    })
+});
