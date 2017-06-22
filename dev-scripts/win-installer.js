a22deb0d7f2d23a1ac625ba607d16740402e92f7
Rohail Najam <rohail_najam@hotmail.com>
2017-06-22 17:03:17 +0500

    fixed bugs

--- dev-scripts/win-installer.js
+++ dev-scripts/win-installer.js
@@ -7,10 +7,9 @@ const electronInstaller = require('electron-winstaller');
 
 let resultPromise = electronInstaller.createWindowsInstaller({
   appDirectory: './build/graph-forest-electron-win32-x64',
-  outputDirectory: './build/installer64',
+  outputDirectory: './build/setup',
   authors: 'Rohail Najam.',
   name : 'Graph-forest',
-  exe : 'graph-forest-electron.exe',
   setupIcon : './app/images/vdlogo.ico'
 });
 
